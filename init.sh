664d2c054e05326376c6e8b335bd5e30c44d0135
Cyanogenom <Cyanogenom@yandex.ru>
2016-11-12 22:27:07 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo pip install --upgrade django==1.9.4
+sudo pip install --upgrade gunicorn==19.4.5
+# sudo mv /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
